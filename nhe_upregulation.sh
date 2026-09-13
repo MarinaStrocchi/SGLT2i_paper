14cb1b961e0063c9d48a450610e4b77a74b1e225
MarinaStrocchi <marina.strocchi@gmail.com>
2026-09-13 06:57:36 +0200

    added more nhe upregulation simulations

--- nhe_upregulation.sh
+++ nhe_upregulation.sh
@@ -73,3 +73,21 @@ cmd="python run_SS_simulation_Tref_pH_R2.py
 		--visualise 
 		>/dev/null 2>&1 &"
 eval $cmd
+
+SIMFOLDER="HF_NHE20"
+cmd="python run_SS_simulation_Tref_pH_R2.py 
+		--simfolder ${SIMFOLDER} 
+		--nhe_factor 20.0 
+		--bicarb_factor 1.0 
+		--Km_Na_factor 1.3
+		--iNa_b_scale_factor 3.0
+		--iK_b_scale_factor 1.0
+		--H_constant -0.0006
+		--pH_e 7.4 
+		--figname ${SIMFOLDER}/results.png 
+		--bcl 1000
+		--nbeats ${NBEATS} 
+		--dtout 1.0 
+		--visualise 
+		>/dev/null 2>&1 &"
+eval $cmd
